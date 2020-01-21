3c2d5ddf19c36b2be0cbb86502fca730b9b5c7a7
valento45 <54119744+valento45@users.noreply.github.com>
2020-01-20 23:59:19 -0300

    Alterado o framework de 4.7.2 para 4.5.2

--- TesteUnitarios/TCadastroUsuario.cs
+++ TesteUnitarios/TCadastroUsuario.cs
@@ -8,24 +8,24 @@ using NUnit.Framework;//chamando a utilização do nunit
 namespace TesteUnitarios
 {
     [TestFixture]
-    public class TCadastroUsuario
-    {
-        private JOALHERIA.BLL.UsuarioBLL usuarioBLL = new JOALHERIA.BLL.UsuarioBLL();
-        private JOALHERIA.DAL.UsuarioDAL usuarioDAL = new JOALHERIA.DAL.UsuarioDAL();
+    public class TCadastroUsuario { 
+    //{
+    //    //private JOALHERIA.BLL.UsuarioBLL usuarioBLL = new JOALHERIA.BLL.UsuarioBLL();
+    //    //private JOALHERIA.DAL.UsuarioDAL usuarioDAL = new JOALHERIA.DAL.UsuarioDAL();
 
-        [Test]  //é a indicação de que a classe abaixo é uma classe que contém casos de teste.
-        public void TestarCadastro()
-        {
-            Boolean result = usuarioDAL.TEfetuarCadastro();
-            Assert.AreEqual(true, result);            
-        }
+    //    [Test]  //é a indicação de que a classe abaixo é uma classe que contém casos de teste.
+    //    public void TestarCadastro()
+    //    {
+    //        Boolean result = usuarioDAL.TEfetuarCadastro();
+    //        Assert.AreEqual(true, result);            
+    //    }
 
-        [Test] //é utilizado para fazer com que o método que vem abaixo se torne um método de teste
-        public void TestarCadastro2()
-        {
-            Boolean result = usuarioDAL.TEfetuarCadastro();
-            Assert.AreEqual(false, result);            
-        }
+    //    [Test] //é utilizado para fazer com que o método que vem abaixo se torne um método de teste
+    //    public void TestarCadastro2()
+    //    {
+    //        Boolean result = usuarioDAL.TEfetuarCadastro();
+    //        Assert.AreEqual(false, result);            
+    //    }
 
     }//
 }//
