bf76918179ca44f6aeab6229fa90694af1f7ac83
valento45 <iggor1935@hotmail.com>
2020-04-01 23:03:33 -0300

    Estudos - Exceptions

---
+++ Estudos/Banco/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estudos.Banco.Entities.Exceptions
+{
+    class DomainException : ApplicationException
+    {
+        public DomainException(string message) : base(message)
+        {
+
+        }
+    }
+}
