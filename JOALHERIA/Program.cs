6f4c77460e627bf26368011c2e5954417bd52c6d
valento45 <iggor1935@hotmail.com>
2020-06-01 20:45:48 -0300

    log capturado na thread de program.cs

--- JOALHERIA/Program.cs
+++ JOALHERIA/Program.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using JOALHERIADAL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -14,9 +16,17 @@ namespace JOALHERIA
         [STAThread]
         static void Main()
         {
+            Application.ThreadException += new ThreadExceptionEventHandler(Application_ThreadException);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new UI.frmLogin());
+        }
+
+        static void Application_ThreadException(object sender, ThreadExceptionEventArgs e)
+        {
+            // Log the exception, display it, etc
+            NetworkLog.Insert(e.Exception, "Program.cs");
+            MessageBox.Show("Ocorreu um erro inesperado!\r\n\r\n\r\n" + e.Exception.Message, "Ops!!!", MessageBoxButtons.OK, MessageBoxIcon.Error);
         }
     }
 }
